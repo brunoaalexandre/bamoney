92432a4d6bea89dbfd405522e5913f864d57db1f
Bruno Alexandre <brunoalencaralexandre@gmail.com>
2022-01-30 14:34:19 -0300

    Global Styles and Editor Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import styled from "styled-components";
+import { GlobalStyle } from "./styles/global";
 
 const Title = styled.h1`
   color: #8257e6;
 `
 
 export function App() {
-  return(
-    <Title>Rolou muito!</Title>
+  return (
+    <>
+      <Title>Rolou muito!</Title>
+      <GlobalStyle />
+    </>
   );
 }
